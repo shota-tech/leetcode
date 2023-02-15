b069daefe79c666a30af38f2aec0d0d52a88cbfb
shota-tech <iwmtsht@gmail.com>
2023-02-15 22:41:27 +0900

    add problem no.11

---
+++ python/two_pointers/11_container_with_most_water.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/container-with-most-water/
+
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        l = 0
+        r = len(height) - 1
+        res = 0
+
+        while l < r:
+            res = max(res, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return res
