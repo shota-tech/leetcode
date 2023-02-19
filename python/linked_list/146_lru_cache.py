5ab3ed647c1685c93011e752cb5dcff8c0d3fd49
shota-tech <iwmtsht@gmail.com>
2023-02-20 00:01:17 +0900

    add problem no.146

---
+++ python/linked_list/146_lru_cache.py
@@ -0,0 +1,60 @@
+# https://leetcode.com/problems/lru-cache/
+
+class Node:
+    def __init__(self, key: int, val: int):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+        self.left = Node(0, 0)
+        self.right = Node(0, 0)
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    def remove(self, node: Node) -> None:
+        prev = node.prev
+        nxt = node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def insert(self, node: Node) -> None:
+        prev = self.right.prev
+        prev.next = node
+        node.prev = prev
+
+        nxt = self.right
+        nxt.prev = node
+        node.next = nxt
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self.remove(node)
+            self.insert(node)
+            return node.val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+
+        node = Node(key, value)
+        self.cache[key] = node
+        self.insert(node)
+
+        if len(self.cache) > self.cap:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
