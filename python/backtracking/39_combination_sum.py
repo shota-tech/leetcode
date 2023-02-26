06ae9c3bf1b7bb7ab99618cd39cf1116a44fb77d
shota-tech <iwmtsht@gmail.com>
2023-02-26 09:40:13 +0900

    refactor

--- python/backtracking/39_combination_sum.py
+++ python/backtracking/39_combination_sum.py
@@ -4,17 +4,15 @@ class Solution:
     def combinationSum(self, candidates: list[int], target: int) -> list[list[int]]:
         res = []
 
-        def backtrack(i: int, current: list[int], total: int):
+        def backtrack(i: int, path: list[int], total: int):
             if total == target:
-                res.append(current.copy())
+                res.append(path)
                 return
             if i >= len(candidates) or total > target:
                 return
 
-            current.append(candidates[i])
-            backtrack(i, current, total + candidates[i])
-            current.pop()
-            backtrack(i + 1, current, total)
+            backtrack(i, path + [candidates[i]], total + candidates[i])
+            backtrack(i + 1, path, total)
 
         backtrack(0, [], 0)
         return res
