5b9182010786dbd74135a042fd1f68092304b1a8
shota-tech <iwmtsht@gmail.com>
2023-02-28 21:39:45 +0900

    add problem no.210

---
+++ python/graphs/210_course_schedule_ii.py
@@ -0,0 +1,32 @@
+# https://leetcode.com/problems/course-schedule-ii/
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: list[list[int]]) -> list[int]:
+        prereq = {i: [] for i in range(numCourses)}
+        for course, pre in prerequisites:
+            prereq[course].append(pre)
+
+        res = []
+        visit = set()
+        path = set()
+
+        def dfs(course: int) -> bool:
+            if course in visit:
+                return True
+            if course in path:
+                return False
+
+            path.add(course)
+            for pre in prereq[course]:
+                if not dfs(pre):
+                    return False
+            path.remove(course)
+
+            visit.add(course)
+            res.append(course)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        return res
