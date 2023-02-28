217cc01e8e773303789822658939125f581476d1
shota-tech <iwmtsht@gmail.com>
2023-02-28 22:32:36 +0900

    add problem no.684

---
+++ python/graphs/684_redundant_connection.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/redundant-connection/
+
+class Solution:
+    def findRedundantConnection(self, edges: list[list[int]]) -> list[int]:
+        parents = [i for i in range(len(edges) + 1)]
+        ranks = [1] * (len(edges) + 1)
+
+        def find(n: int):
+            p = parents[n]
+            while p != parents[p]:
+                parents[p] = parents[parents[p]]
+                p = parents[p]
+            return p
+
+        def union(n1: int, n2: int) -> bool:
+            p1 = find(n1)
+            p2 = find(n2)
+
+            if p1 == p2:
+                return False
+
+            if ranks[p1] > ranks[p2]:
+                parents[p2] = p1
+                ranks[p1] += ranks[p2]
+            else:
+                parents[p1] = p2
+                ranks[p2] += ranks[p1]
+
+            return True
+
+        for n1, n2 in edges:
+            if not union(n1, n2):
+                return [n1, n2]
